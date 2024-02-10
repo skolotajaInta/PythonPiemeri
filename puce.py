69637cd6772dec04779b3c730216b3923375c84b
skolotajaInta <94220354+skolotajaInta@users.noreply.github.com>
2024-02-10 11:17:58 +0000

    Pūce

---
+++ puce.py
@@ -0,0 +1,17 @@
+import os
+from time import sleep
+
+print("\t  Sveiks!")
+print()
+print("Lai tev jautra diena!")
+
+sleep(3)
+os.system("clear")
+
+print("\n\n\n\t|\\_______/|")
+print("\t|         |")
+print("\t| [o] [o] |")
+print("\t|    V    |")
+print("\t|         |")
+print("\t|         |")
+print("\t-ooo---ooo-")
